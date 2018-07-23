e489976e38b2d4510bd90a329ac3af7f553a7187
Carlos Dordelly <cdordelly09@gmail.com>
2018-07-23 11:05:19 -0400

    Null PSR ops.

--- references/connect objects_v02.py
+++ references/connect objects_v02.py
@@ -34,10 +34,6 @@ def join_objects(root, doc, merge_tags):
         c4d.MODELINGCOMMANDMODE_ALL, settings, doc)
     obj = result[0]
 
-    print obj
-
-    print 'Checkpoint 02'
-
     # delete selection tags
     if merge_tags == False:
         obj_tags = obj.GetTags()
@@ -62,37 +58,74 @@ def main():
     # get selected objecs
     objs = get_sel_objects()
     if not objs:
-        print 'no hay objetos' ; return
+        print 'no objects selected.' ; return
 
-    print "checkpoint 00"
-
-    print objs
-
-    print "checkpoint 01"
     null = create_c4d_obj(c4d.Onull, 'main_root')
-    for obj in objs:
-        print obj
-        obj.InsertUnder(null)
 
-    print null
+    # get null center position based form all objects
+    # PSR obj lists
+    list_pos_X = []
+    list_pos_Y = []
+    list_pos_Z = []
+
+    list_rot_X = []
+    list_rot_Y = []
+    list_rot_Z = []
+
+    for obj in objs:
+        list_pos_X.append(obj[c4d.ID_BASEOBJECT_REL_POSITION,c4d.VECTOR_X])
+        list_pos_Y.append(obj[c4d.ID_BASEOBJECT_REL_POSITION,c4d.VECTOR_Y])
+        list_pos_Z.append(obj[c4d.ID_BASEOBJECT_REL_POSITION,c4d.VECTOR_Z])
+
+        list_rot_X.append(obj[c4d.ID_BASEOBJECT_REL_ROTATION,c4d.VECTOR_X])
+        list_rot_Y.append(obj[c4d.ID_BASEOBJECT_REL_ROTATION,c4d.VECTOR_Y])
+        list_rot_Z.append(obj[c4d.ID_BASEOBJECT_REL_ROTATION,c4d.VECTOR_Z])
 
     nP = null[c4d.ID_BASEOBJECT_ABS_POSITION]
     nR = null[c4d.ID_BASEOBJECT_ABS_ROTATION]
 
-    print nP
-    print nR
+    px = 0 ; py = 0 ; pz = 0
+    rx = 0 ; ry = 0 ; rz = 0
 
+    for pos in list_pos_X:
+        px += pos
+
+    for pos in list_pos_Y:
+        py += pos
+
+    for pos in list_pos_Z:
+        pz += pos
+
+    for rot in list_rot_X:
+        rx += rot
+
+    for rot in list_rot_Y:
+        ry += rot
+
+    for rot in list_rot_Z:
+        rz += rot
+
+    px = px / len(list_pos_X) ; py = py / len(list_pos_Y) ; pz = pz / len(list_pos_Z)
+    rx = rx / len(list_rot_X) ; ry = ry / len(list_rot_Y) ; rz = rz / len(list_rot_Z)
+
+    # assign new PSR null value
+    nP = c4d.Vector(px, py, pz)
+    nR = c4d.Vector(rx, ry, rz)
+
+    null[c4d.ID_BASEOBJECT_ABS_POSITION] = c4d.Vector(px, py, pz)
+    null[c4d.ID_BASEOBJECT_ABS_ROTATION] = c4d.Vector(rx, ry, rz)
+
+    c4d.EventAdd()
+
+    # insert all objects inside the root null
+    for obj in objs:
+        obj.InsertUnder(null)
+
+    # collapse and delete old objs
     obj_collapse = join_objects(null, doc, merge_tags)
     null.Remove()
 
-    #obj_collapse = join_objects(objs[0], doc, merge_tags)
-
-    print "checkpoint 03"
-
-    print obj_collapse
-
-    print 'checkpoint 04'
-
+    # update the scene
     c4d.EventAdd()
 
 # ----------------
